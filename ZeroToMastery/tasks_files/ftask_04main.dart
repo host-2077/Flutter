5b74b29523546290866b44bf05ab8bb2c29f1878
Ivan Robayo <robayoivan2077@gmail.com>
2023-05-03 11:40:46 -0500

    DIABLO Tablet

--- ZeroToMastery/tasks_files/ftask_04main.dart
+++ ZeroToMastery/tasks_files/ftask_04main.dart
@@ -25,69 +25,71 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     final carta1 = CartaDePreguntas(
       categoria: "Sports",
-      pregunta: "¿What is one of the newest Olympic sports that exist?",
+      pregunta: "What is one of the newest Olympic sports that exist?",
     );
     final carta2 = CartaDePreguntas(
       categoria: "Comics",
-      pregunta: "¿What's the name of a latinamerican DC villain?",
+      pregunta: "What's the name of a latinamerican DC villain?",
     );
     final carta3 = CartaDePreguntas(
       categoria: "Tech",
-      pregunta: "¿What is the best programming langauge to create apps?",
+      pregunta: "What is the best programming langauge to create apps?",
     );
     final carta4 = CartaDePreguntas(
-      categoria: "Tech",
-      pregunta: "¿What is the best programming langauge to create apps?",
+      categoria: "Sports",
+      pregunta:
+          "What do you call it when a bowler makes three strikes in a row?",
     );
     final carta5 = CartaDePreguntas(
-      categoria: "Tech",
-      pregunta: "¿What is the best programming langauge to create apps?",
+      categoria: "Comics",
+      pregunta: "Who is the fastest DC character?",
     );
     final carta6 = CartaDePreguntas(
       categoria: "Tech",
-      pregunta: "¿What is the best programming langauge to create apps?",
+      pregunta: "One gigabyte is equal to how many megabytes?",
     );
     final carta7 = CartaDePreguntas(
-      categoria: "Tech",
-      pregunta: "¿What is the best programming langauge to create apps?",
+      categoria: "Sports",
+      pregunta: "Which boxer fought against Muhammad Ali and won?",
     );
     final carta8 = CartaDePreguntas(
-      categoria: "Tech",
-      pregunta: "¿What is the best programming langauge to create apps?",
+      categoria: "Comics",
+      pregunta: "Who is the tyrannical ruler of the planet Apokolips?",
     );
     final carta9 = CartaDePreguntas(
       categoria: "Tech",
-      pregunta: "¿What is the best programming langauge to create apps?",
+      pregunta: "What does CPU stand for?",
     );
     final carta10 = CartaDePreguntas(
-      categoria: "Tech",
-      pregunta: "¿What is the best programming langauge to create apps?",
+      categoria: "Sports",
+      pregunta:
+          "In motor racing, what color is the flag they wave to indicate the winner?",
     );
     final carta11 = CartaDePreguntas(
-      categoria: "Tech",
-      pregunta: "¿What is the best programming langauge to create apps?",
+      categoria: "Comics",
+      pregunta: "How did Deathstroke get his powers?",
     );
     final carta12 = CartaDePreguntas(
       categoria: "Tech",
-      pregunta: "¿What is the best programming langauge to create apps?",
+      pregunta: "What does “HTTP” stand for?",
     );
     final carta13 = CartaDePreguntas(
-      categoria: "Tech",
-      pregunta: "¿What is the best programming langauge to create apps?",
+      categoria: "Sports",
+      pregunta: "How many holes are played in an average round of golf?",
     );
     final carta14 = CartaDePreguntas(
-      categoria: "Tech",
-      pregunta: "¿What is the best programming langauge to create apps?",
+      categoria: "Comics",
+      pregunta: "Dick Grayson was first known as Robin and later as...?",
     );
     final carta15 = CartaDePreguntas(
       categoria: "Tech",
-      pregunta: "¿What is the best programming langauge to create apps?",
+      pregunta: "What is a Fat Arrow Notation in Dart?",
     );
 
     return Scaffold(
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       appBar: AppBar(
-        title: const Text("Trivia Maker 1.1"),
+        title: const Text("Trivia App 1.1"),
       ),
       body: Center(
         child: SingleChildScrollView(
@@ -206,7 +208,7 @@ class Home extends StatelessWidget {
                 onTap: () {
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
-                      content: Text("Flutter"),
+                      content: Text("Turkey"),
                     ),
                   );
                 },
@@ -235,7 +237,7 @@ class Home extends StatelessWidget {
                 onTap: () {
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
-                      content: Text("Flutter"),
+                      content: Text("The Flash"),
                     ),
                   );
                 },
@@ -257,11 +259,14 @@ class Home extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 20,
+              ),
               GestureDetector(
                 onTap: () {
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
-                      content: Text("Flutter"),
+                      content: Text("1000"),
                     ),
                   );
                 },
@@ -283,11 +288,14 @@ class Home extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 20,
+              ),
               GestureDetector(
                 onTap: () {
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
-                      content: Text("Flutter"),
+                      content: Text("Joe Frazier"),
                     ),
                   );
                 },
@@ -309,11 +317,14 @@ class Home extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 20,
+              ),
               GestureDetector(
                 onTap: () {
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
-                      content: Text("Flutter"),
+                      content: Text("Darkseid"),
                     ),
                   );
                 },
@@ -335,11 +346,14 @@ class Home extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 20,
+              ),
               GestureDetector(
                 onTap: () {
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
-                      content: Text("Flutter"),
+                      content: Text("Central Processing Unit"),
                     ),
                   );
                 },
@@ -361,11 +375,14 @@ class Home extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 20,
+              ),
               GestureDetector(
                 onTap: () {
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
-                      content: Text("Flutter"),
+                      content: Text("Checkered flag"),
                     ),
                   );
                 },
@@ -387,11 +404,14 @@ class Home extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 20,
+              ),
               GestureDetector(
                 onTap: () {
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
-                      content: Text("Flutter"),
+                      content: Text("A military experiment"),
                     ),
                   );
                 },
@@ -413,11 +433,14 @@ class Home extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 20,
+              ),
               GestureDetector(
                 onTap: () {
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
-                      content: Text("Flutter"),
+                      content: Text("Hypertext Transfer Protocol"),
                     ),
                   );
                 },
@@ -439,11 +462,14 @@ class Home extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 20,
+              ),
               GestureDetector(
                 onTap: () {
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
-                      content: Text("Flutter"),
+                      content: Text("18"),
                     ),
                   );
                 },
@@ -465,11 +491,14 @@ class Home extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 20,
+              ),
               GestureDetector(
                 onTap: () {
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
-                      content: Text("Flutter"),
+                      content: Text("Nightwing"),
                     ),
                   );
                 },
@@ -491,11 +520,14 @@ class Home extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 20,
+              ),
               GestureDetector(
                 onTap: () {
                   ScaffoldMessenger.of(context).showSnackBar(
                     const SnackBar(
-                      content: Text("Flutter"),
+                      content: Text("=>"),
                     ),
                   );
                 },
@@ -515,6 +547,17 @@ class Home extends StatelessWidget {
                       const SizedBox(),
                     ],
                   ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                color: Colors.amberAccent,
+                width: MediaQuery.of(context).size.width,
+                height: 100,
+                child: const Center(
+                  child: Text("MediaQuery Example"),
                 ),
               ),
             ],
